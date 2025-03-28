3016fb4be5fc3b158e2e9ce64c92e76067985370
Amir <amir.naji@vividworks.com>
2025-03-28 13:18:42 +0200

    Changes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -26,18 +26,7 @@ function handleClick(side, action) {
     <React.Fragment>
         <Button text="Hajime" onClick={() => startClick()}/>
         <Button text="Soremade" onClick={() => endClick()}/>
-        <div className='rowC'>
-            <div className='margin'>
-                <Button color="white" text="attack" onClick={()=> handleClick("white", "attack")}/>
-                <Button color="white" text="shido" onClick={()=> handleClick("white", "shido")}/>
-                <Button color="white" text="throw" onClick={()=> handleClick("white", "throw")}/>
-            </div>
-            <div>
-                <Button color="blue" text="attack" onClick={()=> handleClick("blue", "attack")}/>
-                <Button color="blue" text="shido" onClick={()=> handleClick("blue", "shido")}/>
-                <Button color="blue" text="throw" onClick={()=> handleClick("blue", "throw")}/>    
-            </div>
-        </div>
+        
         <div>
             { page.map((str, index) => (
                     <div key={index} className="mb-2">{str}</div>

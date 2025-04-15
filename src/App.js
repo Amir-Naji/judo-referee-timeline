e451bd6c355ff8df3c7c805937b83cc0bcc94c87
Amir <amir.naji@vividworks.com>
2025-04-15 23:49:06 +0300

    Removed the unused import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import MainComponent from './components/MainComponent';
-import { useMediaQuery } from 'react-responsive';
 import MediaQuery from 'react-responsive'
 
 function App() {
